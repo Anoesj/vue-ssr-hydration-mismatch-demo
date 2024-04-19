2f96adc7eca3a0fd300c19a9b09e6e4ab13216e8
Anoesj <anoesjsadraee@gmail.com>
2024-04-19 18:37:03 +0200

    Init

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ComponentWithSlot :isLoading="pending">
+      <template #theSlot>
+        Hi, I'm slotted content!
+      </template>
+    </ComponentWithSlot>
+  </div>
+</template>
+
+<script setup>
+  const { pending } = await useFetch('https://dummyjson.com/products/1');
+</script>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ components/ComponentWithSlot.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Transition name="fade">
+      <slot v-if="!isLoading" name="theSlot"></slot>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    isLoading: boolean;
+  }>();
+</script>
